6a2898850dcdf15ae17de2f0a4f6bd65598487a5
boiv66 <vothanhboiloc2409@gmail.com>
2022-10-17 21:26:33 -0700

    first and lastest date range

--- js/about.js
+++ js/about.js
@@ -1,3 +1,14 @@
+
+function displayEarliestAndLatestDate(tweet_array){
+	orderedDates = tweet_array.map(a => a.time).sort(function(a,b){return Date.parse(a) > Date.parse(b)});
+	document.getElementById('lastDate').innerText = orderedDates[0].toDateString();
+	document.getElementById('firstDate').innerText = orderedDates[orderedDates.length-1].toDateString(); 
+}
+
+function displayEventPercentage(tweet_array){
+	event_count = {live_event:0, }
+}
+
 function parseTweets(runkeeper_tweets) {
 	//Do not proceed if no tweets loaded
 	if(runkeeper_tweets === undefined) {
@@ -12,6 +23,14 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	// console.log(tweet_array[1].time); 
+	// orderedDates = tweet_array.sort(function(a,b){
+	// 	return Date.parse(a) > Date.parse(b)});
+	// console.log(orderedDates)
+	displayEarliestAndLatestDate(tweet_array);
+	displayEventPercentage(tweetarray); 
+	
 }
 
 //Wait for the DOM to load
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -1,50 +1,63 @@
 class Tweet {
-	private text:string;
-	time:Date;
+  private text: string;
+  time: Date;
 
-	constructor(tweet_text:string, tweet_time:string) {
-        this.text = tweet_text;
-		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
-	}
+  constructor(tweet_text: string, tweet_time: string) {
+    this.text = tweet_text;
+    this.time = new Date(tweet_time); //, "ddd MMM D HH:mm:ss Z YYYY"
+  }
 
-	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
-    get source():string {
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+  //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
+  get source(): string {
+    //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
+    if (
+      this.text.startsWith("Just completed a") ||
+      this.text.startsWith("Just posted a")
+    ) {
+      return "complete_event";
+    }
+    if (this.text.startsWith("Watch") && this.text.includes("now")) {
+      return "live_event";
+    }
+    if (this.text.toLowerCase().includes("achieved")) {
+      return "achievement";
     }
 
-    //returns a boolean, whether the text includes any content written by the person tweeting.
-    get written():boolean {
-        //TODO: identify whether the tweet is written
-        return false;
-    }
+    return "miscellaneous";
+  }
 
-    get writtenText():string {
-        if(!this.written) {
-            return "";
-        }
-        //TODO: parse the written text from the tweet
-        return "";
-    }
+  //returns a boolean, whether the text includes any content written by the person tweeting.
+  get written(): boolean {
+    //TODO: identify whether the tweet is written
+    return false;
+  }
 
-    get activityType():string {
-        if (this.source != 'completed_event') {
-            return "unknown";
-        }
-        //TODO: parse the activity type from the text of the tweet
-        return "";
+  get writtenText(): string {
+    if (!this.written) {
+      return "";
     }
+    //TODO: parse the written text from the tweet
+    return "";
+  }
 
-    get distance():number {
-        if(this.source != 'completed_event') {
-            return 0;
-        }
-        //TODO: prase the distance from the text of the tweet
-        return 0;
+  get activityType(): string {
+    if (this.source != "completed_event") {
+      return "unknown";
     }
+    //TODO: parse the activity type from the text of the tweet
+    return "";
+  }
 
-    getHTMLTableRow(rowNumber:number):string {
-        //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
-        return "<tr></tr>";
+  get distance(): number {
+    if (this.source != "completed_event") {
+      return 0;
     }
+    //TODO: prase the distance from the text of the tweet
+    return 0;
+  }
+
+  getHTMLTableRow(rowNumber: number): string {
+    //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
+    return "<tr></tr>";
+  }
 }
